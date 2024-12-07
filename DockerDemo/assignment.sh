496aaf791f44c7713bf7194ceca635d235f9a25d
h4ckable <hockeyfreak908@gmail.com>
2024-12-07 03:07:27 +0000

    Updated Dockerfile and assignment.sh

--- DockerDemo/assignment.sh
+++ DockerDemo/assignment.sh
@@ -1,1 +1,11 @@
-Hello World
+#!/bin/bash
+
+apt update
+apt install -y nginx
+apt install -y vim
+service nginx start
+
+sed -i "s/Welcome to nginx/Welcome to Jarren's page/" /var/www/html/*.html
+sed -i 's/nginx/nxginx (pronounced as EngineX)/' /var/www/html/*.html
+
+
